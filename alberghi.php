4d27dcae2fea07300cd283e6f1ea88be8140c562
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-12-18 15:14:58 +0100

    sv

--- alberghi.php
+++ alberghi.php
@@ -39,18 +39,20 @@
         ],
 
     ];
-    foreach ($hotels as $lista){
-            echo $lista;
-        };
     ?> 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1><?php echo $lista["name"]?></h1>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        foreach ($hotels as $lista){
+            echo $lista;
+            <h1><?php echo $lista["vote"]?></h1>
+            };
+        ?>
 </body>
 </html>
